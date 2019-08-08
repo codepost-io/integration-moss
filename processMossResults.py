1c6ccda4827a46bfda7a69fa63d574c41349c404
vayyala <vayyala08@gmail.com>
2019-08-07 22:43:52 -0400

    Initialize files

---
+++ processMossResults.py
@@ -0,0 +1,73 @@
+#  Usage: python3 processMossResults.py MOSSURL threshold
+#
+#  NOTE: This script only works if the files uploaded to MOSS are of the format
+#        '/tmp/<submissionID>_<student(s)>/<fileName>'
+#
+#  This script takes a MOSS URL and a similarity threshold for which to report plagiarism and:
+#     1. Gets the results from the <MOSS URL>, which compares items with the syntax:
+#           '/tmp/<submissionID>_<student(s)>/<fileName>'
+#     2. Processes the results to find the set of (submissionID1, submissionID2, similarity) tuples
+#        where similarity >= threshold
+#     3. Adds a comment to the first files of submissionID1 and submissionID2 with a comment to
+#       flag the detected plagiarism
+
+import pandas as pd
+import re
+import argparse
+import codepost
+
+codepost.configure_api_key("<YOUR API KEY HERE>")
+
+##################### Argument Parsing ######################################################
+parser = argparse.ArgumentParser(description='Working with MOSS!')
+
+# argument parser for similarity thresholds
+def restricted_float(x):
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+    return x
+
+parser.add_argument('mossURL', help='URL of MOSS Results')
+parser.add_argument('threshold', type=restricted_float, help='A percentage similarity threshold. e.g., 50% similarity, put in a threshold of .5')
+
+args = parser.parse_args()
+
+##################### Process Moss results  ######################################################
+# Get results from a mossURL and output tutples of (submissionID1, submissionID2, similarity)
+def readResults(mossURL, threshold):
+    dfs = pd.read_html(mossURL, header=0)
+    similarSubmissions = []
+    for index, row in dfs[0].iterrows():
+        similarity = re.search(r'(?<=\()(\d*?)(?=%)', row['File 1']).group()
+        submissionID1 = re.search(r'(?<=/)(\d*?)(?=_)', row['File 1']).group()
+        submissionID2 = re.search(r'(?<=/)(\d*?)(?=_)', row['File 2']).group()
+        if(float(similarity)/100 >= threshold):
+            similarSubmissions.append((submissionID1, submissionID2, similarity))
+    return similarSubmissions
+
+def addComment(submissionID1, submissionID2, similarity):
+    """
+    Adds comments to the first file of a submission, noting that there was plagiarism detected in this submission
+    """
+    sub = codepost.submission.retrieve(id=int(submissionID1))  # get the student's submission
+    fileID = sub.files[0]    # get the id of the first file from the submissions
+
+    # create a comment on the first file of that submission
+    my_comment = codepost.comment.create(
+      text="FLAG: High level of code similarity with another submission. {}% similarity with submission id {}".format(similarity, submissionID2),
+      startChar=1,
+      endChar=1,
+      startLine=1,
+      endLine=2,
+      file=fileID,
+      pointDelta=0,
+      rubricComment=None)
+
+    print("Comment added to submission. Visit the URL here to view: codepost.io/code/{}".format(submissionID1))
+
+##################### Main  ######################################################
+similarResults = readResults(args.mossURL, args.threshold)
+for (submissionID1, submissionID2, similarity) in similarResults:
+    addComment(submissionID1, submissionID2, similarity)
+    addComment(submissionID2, submissionID1, similarity)
---
+++ sendToMoss.py
@@ -0,0 +1,87 @@
+#  NOTE: In order to run this script, you must have the moss.pl file in the same diretory.
+#        You can get this by reaching out to moss@moss.stanford.edu with the following email content:
+#               registeruser
+#               email
+#  NOTE: Before running this, please edit the codePost API key, courseName, and coursePeriod constants
+#
+#
+#  Usage: python3 sendToMoss.py assignmentName [-m] "[optional MOSS arguments]"
+#
+#    assignmentName is the name of the assignment on codePost to check code similarity for
+#
+#    There are other moss arguments that you can pass in after -m (in quotes).
+#    These can be seen in the moss.pl documentation.
+#
+#    For example, you can do the following:
+#       python3 sendToMoss.py assignmentName -m "-l java"
+#    This will tell moss that you are sending .java files. When it knows the language,
+#       the plagiarism detection will be more accurate vs. parsing as text files.
+#
+#    The script does the following:
+#       1. Gets all the submissions of <assignmentName> and saves them in a temp directory,
+#           in the format /tmp/<submissionID>_<students/*
+#       2. Sends the submissions to MOSS along with the optional MOSS arguments
+#       3. Prints out the MOSS result link in the console
+
+import argparse
+import codepost
+import subprocess
+import os
+import shutil
+
+#################### Constants -- PLEASE EDIT THESE ########################################
+codepost.configure_api_key("<YOUR API KEY HERE>")
+courseName="<YOUR CODEPOST COURSE NAME HERE>"
+coursePeriod="<YOUR CODEPOST COURSE PERIOD HERE>"
+
+##################### Argument Parsing ######################################################
+parser = argparse.ArgumentParser(description='Working with MOSS!')
+parser.add_argument('assignmentName', help='Assignment Name')
+parser.add_argument('-m','--m', nargs='*', help='Optional moss arguments')
+args = parser.parse_args()
+
+##################### Helper Functions ######################################################
+# get the assignment from codePost based on the couseName, coursePeriod, and assignmentName
+def getAssignment(courseName, coursePeriod, assignmentName):
+    courses = list(codepost.course.list_available(name=courseName, period=coursePeriod))
+    if len(courses) == 0:
+        raise Exception("Course does not exist!")
+    course = courses[0]
+
+    assignments = [codepost.assignment.retrieve(id=a_id) for a_id in course.assignments]
+    assignment = [a for a in assignments if a.name == assignmentName]
+    if len(assignment) == 0:
+        raise Exception("Assignment does not exist!")
+    return assignment[0]
+
+# Get all the submissions of the given assignment, and save them to a local temp folder
+def getSubmissions(assignmentID):
+    submissions = codepost.assignment.list_submissions(id=assignmentID)
+    os.mkdir('tmp')
+    for s in submissions:
+        folderName = '{}_'.format(s.id) + ''.join(s.students)
+        os.mkdir('tmp/{}'.format(folderName))
+        for fID in s.files:
+            f = codepost.file.retrieve(id=fID)
+            file = open('tmp/{}/{}'.format(folderName,f.name), "w")
+            file.write(f.code)
+            file.close()
+
+# Send the submissions to MOSS
+# If everything works correctly, a link will be outputted to the console
+def runMossCheck():
+    directories = [x[0]+'/*' for x in os.walk('./tmp') if x[0] != './tmp']
+    if(args.m is not None):
+        subprocess.call('./moss '+" ".join(args.m)+" -d "+" ".join(directories), shell=True)
+    else:
+        subprocess.call('./moss -d '+" ".join(directories), shell=True)
+
+
+##################### Main  ######################################################
+assignment = getAssignment(courseName, coursePeriod, args.assignmentName)
+subs = getSubmissions(assignment.id)
+try:
+    runMossCheck()
+    shutil.rmtree('./tmp') # delete tmp folder when complete
+except:
+    shutil.rmtree('./tmp') # delete tmp folder when complete
